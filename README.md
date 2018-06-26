c0ba5c694743455689f5c50a261266f2583dddd5
Steve Kluck <steve@kluck.engineering>
2018-06-25 22:45:43 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Serverless Framework + Lambda Starter Kit
+
+- Serverless Framework 1.x
+- AWS Lambda (Node JS 8.10)
+- Webpack 4.x
+- Babel 6.x
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const slsw = require('serverless-webpack');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+
+  mode: 'development',
+
   module: {
     rules: [{
       test: /\.js$/,
